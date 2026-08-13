375c55a50844e99282a5b072cff21c25b70167a4
Aryan Singh <aquiveal@gmail.com>
2026-08-13 06:02:03 +0530

    refactor: remove contact selection UI
    Remove the contact search and selection functionality from the campaign
    preview modal. This includes the autocomplete field, related state
    management, and the subscriber_id parameter from the test email API call.
    
    The contact selection feature is being simplified and will be handled
    through a different mechanism.

--- frontend/src/components/CampaignPreview.vue
+++ frontend/src/components/CampaignPreview.vue
@@ -6,20 +6,6 @@
           <header class="modal-card-head" style="justify-content: space-between; align-items: center;">
             <h4>{{ title }}</h4>
             <div style="display: flex; align-items: center; gap: 10px;">
-              <b-field label="Select contact" label-position="on-border" style="margin-bottom:0; min-width: 220px;">
-                <b-autocomplete
-                  v-model="contactSearch"
-                  :data="filteredContacts"
-                  placeholder="Search name, email, phone..."
-                  field="displayLabel"
-                  :loading="isSearchingContacts"
-                  size="is-small"
-                  open-on-focus
-                  @typing="getAsyncContacts"
-                  @select="onSelectContact">
-                  <template #empty>No contacts found</template>
-                </b-autocomplete>
-              </b-field>
               <b-button v-if="id && type === 'template'" size="is-small" type="is-info" icon-left="email-send-outline" @click="sendTest">
                 Send Test
               </b-button>
@@ -34,7 +20,6 @@
             <input v-if="templateType" type="hidden" name="template_type" :value="templateType" />
             <input v-if="archiveMeta" type="hidden" name="archive_meta" :value="archiveMeta" />
             <input v-if="body" type="hidden" name="body" :value="body" />
-            <input v-if="selectedSubscriberId" type="hidden" name="subscriber_id" :value="selectedSubscriberId" />
           </form>
 
           <iframe id="iframe" name="iframe" ref="iframe" :title="title" :src="isPost ? 'about:blank' : previewURL"
@@ -82,11 +67,6 @@ export default {
       isVisible: true,
       isLoading: true,
       formSubmitted: false,
-      selectedSubscriberId: '',
-      contactSearch: '',
-      filteredContacts: [],
-      isSearchingContacts: false,
-      searchDebounceTimer: null,
     };
   },
 
@@ -94,36 +74,6 @@ export default {
     close() {
       this.$emit('close');
       this.isVisible = false;
-    },
-
-    getAsyncContacts(query) {
-      if (this.searchDebounceTimer) {
-        clearTimeout(this.searchDebounceTimer);
-      }
-      this.searchDebounceTimer = setTimeout(() => {
-        this.isSearchingContacts = true;
-        this.$api.getSubscribers({ search: query, per_page: 20 }).then((res) => {
-          this.isSearchingContacts = false;
-          const results = res.results || (res.data ? res.data.results : []) || [];
-          this.filteredContacts = results.map((c) => ({
-            ...c,
-            displayLabel: c.name ? `${c.name} (${c.email || c.phone || c.id})` : `${c.email || c.phone || `ID: ${c.id}`}`,
-          }));
-        }).catch(() => {
-          this.isSearchingContacts = false;
-          this.filteredContacts = [];
-        });
-      }, 300);
-    },
-
-    onSelectContact(option) {
-      if (option && option.id) {
-        this.selectedSubscriberId = option.id;
-        this.reloadPreview();
-      } else {
-        this.selectedSubscriberId = '';
-        this.reloadPreview();
-      }
     },
 
     // On iframe load, kill the spinner.
@@ -151,7 +101,6 @@ export default {
       if (!email) return;
       try {
         await this.$api.post(`/api/templates/${this.id}/test`, {
-          subscriber_id: parseInt(this.selectedSubscriberId, 10) || 0,
           test_email: email,
         });
         this.$buefy.toast.open({ message: 'Test message dispatched successfully', type: 'is-success' });
